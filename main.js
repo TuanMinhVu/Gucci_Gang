737d6d921bf7dba8405d0166bf226d8c138c4d42
Oliver MS <38758255+all-the-right-hype@users.noreply.github.com>
2018-05-15 22:17:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var player = {};
+var playerHealth;
+var playerSpeed;
+
+var pistol = {};
+var pistolBullets = [];
+
+var zombie = {};
+
+var spacebarPressed = false;
+var upPressed = false;
+var downPressed = false;
+var leftPressed = false;
+var rightPressed = false;
+
+var map = [];
